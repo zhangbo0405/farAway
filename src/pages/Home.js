d66769dcee0eadd940c123a2cd7623ecb378c45f
zhangbo0405 <1697815645@qq.com>
2024-10-11 17:37:39 +0800

    changeFile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import RotatingSphere from '../components/RotatingSphere';
 function Home(){
     return <>
     <h1>Three.js: RotatingSphere Pages!</h1>
+    <h3>Three.js:2.0</h3>
     <RotatingSphere></RotatingSphere>
     </>
 }
